ee185bdbcf9cce0a4b44a7cc8dfe8725412ed4c5
Serge1233211 <141172587+Serge1233211@users.noreply.github.com>
2024-04-29 19:00:50 -0400

    Prob5

--- src/Prob5/HolidayScheduleExample.java
+++ src/Prob5/HolidayScheduleExample.java
@@ -9,13 +9,9 @@ public class HolidayScheduleExample {
         // Get the current year
         int currentYear = LocalDate.now().getYear();
 
-        // Define some holidays
+        // Define some holidays (you can add more as needed)
         LocalDate newYearsDay = LocalDate.of(currentYear, Month.JANUARY, 1);
         LocalDate myBirthday = LocalDate.of(currentYear, Month.SEPTEMBER, 27);
-        LocalDate valentinesDay = LocalDate.of(currentYear, Month.FEBRUARY, 14);
-        LocalDate internationalWorkersDay = LocalDate.of(currentYear, Month.MAY, 1);
-        LocalDate halloween = LocalDate.of(currentYear, Month.OCTOBER, 31);
-        LocalDate christmas = LocalDate.of(currentYear, Month.DECEMBER, 25);
 
         // Format the dates
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM d");
@@ -23,9 +19,8 @@ public class HolidayScheduleExample {
         // Print the holiday schedule
         System.out.println("\"New Year's Day\" - " + newYearsDay.format(formatter));
         System.out.println("\"My birthday\" - " + myBirthday.format(formatter));
-        System.out.println("\"Valentine's Day\" - " + valentinesDay.format(formatter));
-        System.out.println("\"International Workers' Day\" - " + internationalWorkersDay.format(formatter));
-        System.out.println("\"Halloween\" - " + halloween.format(formatter));
-        System.out.println("\"Christmas\" - " + christmas.format(formatter));
+        // Add more holidays here...
+
+        // You can continue adding other holidays in a similar format
     }
 }
